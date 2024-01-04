60a60af86e680418db54e7b6636df6c886370448
brenda <bbritob8@gmail.com>
2024-01-04 16:08:05 -0300

    incluindo  base

--- CalculadoraComTDD/Calculadora.cs
+++ CalculadoraComTDD/Calculadora.cs
@@ -2,28 +2,43 @@ namespace CalculadoraComTDD;
 
 public class Calculadora
 {
+    private List<string> historicoList;
+
+    public Calculadora()
+    {
+        historicoList = new List<string>();
+    }
     public int somar(int numero1, int numero2)
     {
-        return 0;
+        int resultado = numero1 + numero2;
+        historicoList.Insert(0, "resultado = " + resultado);
+        return resultado;
     }
     
     public int subtrair(int numero1, int numero2)
     {
-        return 0;
+        int resultado = numero1 - numero2;
+        historicoList.Insert(0, "resultado = " + resultado);
+        return resultado;
     }
     
     public int multiplicar(int numero1, int numero2)
     {
-        return 0;
+        int resultado = numero1 * numero2;
+        historicoList.Insert(0, "resultado = " + resultado);
+        return resultado;
     }
     
     public int dividir(int numero1, int numero2)
     {
-        return 0;
+        int resultado = numero1 / numero2;
+        historicoList.Insert(0, "resultado = " + resultado);
+        return resultado;
     }
 
     public List<string> historico()
     {
-        return new List<string>();
+        historicoList.RemoveRange(3, historicoList.Count - 3);
+        return historicoList;
     }
 }
