500318f71f452539c8d9b006a9e0232dc6983029
brenda <bbritob8@gmail.com>
2024-01-04 15:22:12 -0300

    incluindo base

--- CalculadoraComTDD/Calculadora.cs
+++ CalculadoraComTDD/Calculadora.cs
@@ -2,5 +2,28 @@ namespace CalculadoraComTDD;
 
 public class Calculadora
 {
+    public int somar(int numero1, int numero2)
+    {
+        return 0;
+    }
     
+    public int subtrair(int numero1, int numero2)
+    {
+        return 0;
+    }
+    
+    public int multiplicar(int numero1, int numero2)
+    {
+        return 0;
+    }
+    
+    public int dividir(int numero1, int numero2)
+    {
+        return 0;
+    }
+
+    public List<string> historico()
+    {
+        return new List<string>();
+    }
 }
